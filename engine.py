0e63b10d5e4c4a2f4a28b77e7026feaec33f407a
davidwong <arkwanyuk@gmail.com>
2024-08-15 18:49:38 +0800

    Update eval code

--- engine.py
+++ engine.py
@@ -16,13 +16,19 @@ import utils.loss_utils as loss_utils
 import utils.eval_utils as eval_utils
 
 
-def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable, 
-                    optimizer: torch.optim.Optimizer, device: torch.device, 
-                    epoch: int, max_norm: float = 0):
+def train_one_epoch(
+    args,
+    model: torch.nn.Module,
+    data_loader: Iterable,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+    epoch: int,
+    max_norm: float = 0,
+):
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
+    metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value:.6f}"))
+    header = "Epoch: [{}]".format(epoch)
     print_freq = 10
 
     for batch in metric_logger.log_every(data_loader, print_freq, header):
@@ -41,8 +47,7 @@ def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable,
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
-        loss_dict_reduced_unscaled = {k: v
-                                      for k, v in loss_dict_reduced.items()}
+        loss_dict_reduced_unscaled = {k: v for k, v in loss_dict_reduced.items()}
         losses_reduced_unscaled = sum(loss_dict_reduced_unscaled.values())
         loss_value = losses_reduced_unscaled.item()
 
@@ -50,13 +55,13 @@ def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable,
             print("Loss is {}, stopping training".format(loss_value))
             print(loss_dict_reduced)
             sys.exit(1)
-        
+
         optimizer.zero_grad()
         losses.backward()
         if max_norm > 0:
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
         optimizer.step()
-        
+
         metric_logger.update(loss=loss_value, **loss_dict_reduced_unscaled)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
     # gather the stats from all processes
@@ -70,7 +75,7 @@ def validate(args, model: torch.nn.Module, data_loader: Iterable, device: torch.
     model.eval()
 
     metric_logger = utils.MetricLogger(delimiter="  ")
-    header = 'Eval:'
+    header = "Eval:"
 
     for batch in metric_logger.log_every(data_loader, 10, header):
         img_data, text_data, target = batch
@@ -79,12 +84,12 @@ def validate(args, model: torch.nn.Module, data_loader: Iterable, device: torch.
         img_data = img_data.to(device)
         text_data = text_data.to(device)
         target = target.to(device)
-        
+
         pred_boxes = model(img_data, text_data)
         miou, accu = eval_utils.trans_vg_eval_val(pred_boxes, target)
-        
-        metric_logger.update_v2('miou', torch.mean(miou), batch_size)
-        metric_logger.update_v2('accu', accu, batch_size)
+
+        metric_logger.update_v2("miou", torch.mean(miou), batch_size)
+        metric_logger.update_v2("accu", accu, batch_size)
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
@@ -116,11 +121,10 @@ def evaluate(args, model: torch.nn.Module, data_loader: Iterable, device: torch.
     accu_num = eval_utils.trans_vg_eval_test(pred_boxes, gt_boxes)
 
     result_tensor = torch.tensor([accu_num, total_num]).to(device)
-    
+
     torch.cuda.synchronize()
     dist.all_reduce(result_tensor)
 
     accuracy = float(result_tensor[0]) / float(result_tensor[1])
-    
+
     return accuracy
-        
